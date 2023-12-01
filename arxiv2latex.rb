914a4ca1f0bce807ce8254008b4e9fc01ba838d2
Andrea Pinto <andreakiro.pinto@gmail.com>
2023-12-01 14:51:36 -0500

    bump tap to v1.0.2

--- arxiv2latex.rb
+++ arxiv2latex.rb
@@ -1,8 +1,8 @@
 class Arxiv2latex < Formula
     desc "A simple shell script to download and extract LaTeX source files from arXiv given an arXiv ID or URL"
     homepage "https://github.com/andreakiro/homebrew-arxiv2latex"
-    url "https://github.com/andreakiro/homebrew-arxiv2latex/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "27b5f9ddfb69a88c151177a99a53a9a74669c459d555a41f74ddc01027cfe3e0"
+    url "https://github.com/andreakiro/homebrew-arxiv2latex/archive/refs/tags/v1.0.2.tar.gz"
+    sha256 "fcca959100ba1dc60abcbbd9ff95465d91dc8afe7b697007ac100a185b80d168"
   
     def install
       bin.install "arxiv2latex.sh" => "arxiv2latex"
