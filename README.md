74af4e9984f0805c22f8b4bac1a71615dfcbe21e
Andrea Pinto <andreakiro.pinto@gmail.com>
2023-12-01 13:02:00 -0500

    upload arxiv2latex-sh script

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # arxiv2latex-sh
+
+A simple shell script to download and extract LaTeX source files from arXiv given an arXiv ID or URL. You can pass the abs, pdf or id (versioned as well):
+
+- https://arxiv.org/abs/1706.03762v7
+- https://arxiv.org/pdf/1706.03762.pdf
+- 1706.03762v6
+
+## Usage
+
+```shell
+chmod +x arxiv2latex.sh
+sh arxiv2latex.sh <arxiv-url-or-id>
+```
+
+## Brew
+
+```
+
+```
---
+++ arxiv2latex.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <arxiv-url-or-id>"
+    exit 1
+fi
+
+INPUT=$1
+
+# Extract the arXiv ID with versioning from the input
+if [[ $INPUT =~ [0-9]{4}\.[0-9]{4,5}(v[0-9]+)? ]]; then
+    ARXIV_ID=${BASH_REMATCH[0]}
+else
+    echo "Invalid input. Please provide a valid arXiv URL or ID."
+    exit 1
+fi
+
+D="tmpdir-${ARXIV_ID}"
+DOWNLOAD_URL="https://arxiv.org/e-print/$ARXIV_ID"
+
+mkdir -p $D
+echo "Downloading $DOWNLOAD_URL"
+
+# Download the tar.gz file into the directory with a browser-like user-agent
+wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" -O "$D/$ARXIV_ID.tar.gz" $DOWNLOAD_URL
+
+# Check if wget was successful
+if [ $? -ne 0 ]; then
+    echo "Error downloading file. Please check the arXiv ID or your network connection."
+    rm -rf "$D"  # Clean up by removing the directory
+    exit 1
+fi
+
+cd $D && tar -xvf "$ARXIV_ID.tar.gz"
+rm "$ARXIV_ID.tar.gz" && cd ..
+zip -r "${ARXIV_ID}.zip" "$D"
+rm -rf "$D"
